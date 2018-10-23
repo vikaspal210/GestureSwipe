cc976f392b6709e477acdeb9325673cc899f28c9
Vikas Pal <pal.vikas210@gmail.com>
2018-10-24 00:23:48 +0530

    Added Gesture to VideoView

--- app/src/main/java/com/example/cas/gestureswipe/MainActivity.java
+++ app/src/main/java/com/example/cas/gestureswipe/MainActivity.java
@@ -1,25 +1,54 @@
 package com.example.cas.gestureswipe;
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.session.MediaController;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.cas.gestureswipe.SimpleGestureFilter.SimpleGestureListener;
 
 public class MainActivity extends AppCompatActivity implements
         SimpleGestureListener {
+    //constants
     private SimpleGestureFilter detector;
+    VideoView videoView;
+    MainActivity activity;
+    MediaController mediaController;
+    AudioManager audioManager;
+    int currentPosition;
+    int currentVolume;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView=(ImageView)findViewById(R.id.image_view);
 
         // Detect touched area
         detector = new SimpleGestureFilter(MainActivity.this, this);
+
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        /*//for audio
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT,
+                android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("audio/*");*/
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.glitched_smile;
+        videoView.setVideoURI(Uri.parse(path));
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        videoView.requestFocus();
+        videoView.start();
     }
 
     @Override
@@ -30,28 +59,52 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     @Override
-    public void onSwipe(int direction) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
-        //Detect the swipe gestures and display toast
-        String showToastMessage = "";
+    @Override
+    public void onSwipe(int direction) {
+        // TODO Auto-generated method stub
+        String str = "";
 
         switch (direction) {
 
-            case SimpleGestureFilter.SWIPE_RIGHT:
-                showToastMessage = "You have Swiped Right.";
-                break;
             case SimpleGestureFilter.SWIPE_LEFT:
-                showToastMessage = "You have Swiped Left.";
+
+                currentPosition = videoView.getCurrentPosition();
+                currentPosition = videoView.getCurrentPosition() - 10000;
+                videoView.seekTo(currentPosition);
+                str = "Swipe Left";
                 break;
+
+            case SimpleGestureFilter.SWIPE_RIGHT:
+
+                currentPosition = videoView.getCurrentPosition();
+                currentPosition = videoView.getCurrentPosition() + 10000;
+                videoView.seekTo(currentPosition);
+                str = "Swipe Right";
+                break;
+
             case SimpleGestureFilter.SWIPE_DOWN:
-                showToastMessage = "You have Swiped Down.";
+
+                currentVolume = audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        currentVolume - 1, 0);
+                str = "Swipe Down";
                 break;
             case SimpleGestureFilter.SWIPE_UP:
-                showToastMessage = "You have Swiped Up.";
+
+                currentVolume = audioManager
+                        .getStreamVolume(AudioManager.STREAM_MUSIC);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,
+                        currentVolume + 1, 0);
+                str = "Swipe Up";
                 break;
 
         }
-        Toast.makeText(this, showToastMessage, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 
 
