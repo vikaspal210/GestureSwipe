808b1b0932c59f6fc2642dc8a1b49bbbdce8ff42
Vikas Pal <pal.vikas210@gmail.com>
2018-10-23 22:40:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/cas/gestureswipe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cas.gestureswipe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
